fa9d0c72d5d1ac4aae71c5cbdcd1346125075bdd
eduardonk9999 <thiago.novak7@gmail.com>
2022-08-29 18:50:38 -0300

    projeto um

---
+++ projetoUm/index.js
@@ -0,0 +1,28 @@
+const { gql, ApolloServer } = require("apollo-server")
+
+const typeDefs = gql`
+  type Query {
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
+  }
+`
+const resolvers = {
+  Query: {
+    idade() {
+      return 30;
+    },
+    salario() {
+      return 1000
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen()
